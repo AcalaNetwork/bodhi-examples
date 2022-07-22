4a794535af5d130648b8e34294e3a14e1ea0d40e
Shunji Zhan <shunhahaha@live.com>
2022-07-22 17:03:00 +0800

    basic flow works

--- batch-transactions/src/App.tsx
+++ batch-transactions/src/App.tsx
@@ -10,22 +10,24 @@ import type {
   InjectedAccount,
 } from '@polkadot/extension-inject/types';
 import { Input, Button, Select } from 'antd';
+import {
+  ArrowDownOutlined, ArrowUpOutlined, MinusOutlined, PlusOutlined, StarOutlined,
+} from '@ant-design/icons';
+import { handleTxResponse } from '@acala-network/eth-providers';
+import { getContractAddress } from '@ethersproject/address';
+import Confetti from 'react-confetti';
 
 import {
-  deployUniFactory,
   getAddLiquidityExtrinsic,
   getTokenApproveExtrinsic,
   getTokenDeployExtrinsic,
   getUniFactoryDeployExtrinsic,
   getUniRouterDeployExtrinsic,
-  queryDexLiquidity,
   queryLiquidity,
   queryTokenAllowance,
   queryTokenBalance,
 } from './utils';
 import './App.scss';
-import { handleTxResponse } from '@acala-network/eth-providers';
-import { getContractAddress } from '@ethersproject/address';
 
 const { Option } = Select;
 
@@ -47,18 +49,28 @@ function App() {
   const [provider, setProvider] = useState<Provider | null>(null);
   const [selectedAddress, setSelectedAddress] = useState<string>('');
   const [claimedEvmAddress, setClaimedEvmAddress] = useState<string>('');
-  const [balance, setBalance] = useState<string>('');
   const [uniCoreAddress, setUniCoreAddress] = useState<string>('');
   const [uniRouterAddress, setUniRouterAddress] = useState<string>('');
   const [token0Address, setToken0Address] = useState<string>('');
   const [token1Address, setToken1Address] = useState<string>('');
-  const [token0Balance, setToken0Balance] = useState<string>('');
-  const [token1Balance, setToken1Balance] = useState<string>('');
-  const [token0Allowance, setToken0Allowance] = useState<string>('0');
-  const [token1Allowance, setToken1Allowance] = useState<string>('0');
-  const [liquidity, setLiquidity] = useState<string>('');
+  const [balance, setBalance] = useState<string>('');
+  const [input0, setInput0] = useState<string>('123456');
+  const [input1, setInput1] = useState<string>('888888');
+  const [liquidity, setLiquidity] = useState<string>('0');
   const [url, setUrl] = useState<string>('wss://acala-mandala.api.onfinality.io/public-ws');
   // const [url, setUrl] = useState<string>('ws://localhost:9944');
+
+  // init data
+  const [token0Balance, setToken0Balance] = useState<string>('');
+  const [token1Balance, setToken1Balance] = useState<string>('');
+  const [token0Allowance, setToken0Allowance] = useState<string>('');
+  const [token1Allowance, setToken1Allowance] = useState<string>('');
+
+  // updated data after batch call
+  const [token0NewBalance, setToken0NewBalance] = useState<string>('');
+  const [token1NewBalance, setToken1NewBalance] = useState<string>('');
+  const [token0NewAllowance, setToken0NewAllowance] = useState<string>('');
+  const [token1NewAllowance, setToken1NewAllowance] = useState<string>('');
 
   /* ------------
     Step 1:
@@ -103,7 +115,7 @@ function App() {
   }, [provider, curExtension, selectedAddress]);
 
   /* ----------
-     Step 1.2: locad some info about the account such as:
+     Step 1.2: load some info about the account such as:
      - bound/default evm address
      - balance
      - whatever needed
@@ -130,68 +142,78 @@ function App() {
     }());
   }, [signer]);
 
-  /* ------------ Step 2: deploy contract ------------ */
-  // need to extract some code from signer.sendTransaction
+  /* ------------ Step 2: deploy contracts ------------ */
   const deploy = useCallback(async () => {
-    if (!signer || !provider) return;
+    if (!signer || !provider || !claimedEvmAddress) return;
 
     setDeploying(true);
     try {
-      // TODO: save signer addr
-      const evmAddr = await signer.getAddress();
+      /* ----------
+        Step 2.1:
+          - construct each of the contract deployment extrinsic
+          - batch them into a single `batchAll` extrinsic
+                                                     ---------- */
       const txCount = await signer.getTransactionCount('latest');
-      const predictedUniCoreAddr = getContractAddress({ from: evmAddr, nonce: txCount });
+      const predictedUniCoreAddr = getContractAddress({ from: claimedEvmAddress, nonce: txCount });
 
-      const extrinsic1 = await getUniFactoryDeployExtrinsic(signer, evmAddr);
-      const extrinsic2 = await getUniRouterDeployExtrinsic(signer, predictedUniCoreAddr);
-      const extrinsic3 = await getTokenDeployExtrinsic(signer, [1000000000000]);
-      const extrinsic4 = await getTokenDeployExtrinsic(signer, [2000000000000]);
+      const deployExtrinsics = await Promise.all([
+        getUniFactoryDeployExtrinsic(signer, claimedEvmAddress),
+        getUniRouterDeployExtrinsic(signer, predictedUniCoreAddr),
+        getTokenDeployExtrinsic(signer, [1000000]),
+        getTokenDeployExtrinsic(signer, [2000000]),
+      ]);
 
-      const extrinsic = await provider.api.tx.utility.batchAll([
-        extrinsic1,
-        extrinsic2,
-        extrinsic3,
-        extrinsic4,
-      ]).signAsync(await signer.getSubstrateAddress());
+      const batchDeploy = provider.api.tx.utility.batchAll(deployExtrinsics);
 
-      extrinsic.send((result: SubmittableResult) => {
+      /* ----------
+         Step 2.2:
+           - sign and send the `batchAll` extrinsic
+           - handle result in the callback function
+                                         ---------- */
+      await batchDeploy.signAndSend(selectedAddress, (result: SubmittableResult) => {
         if (result.status.isFinalized || result.status.isInBlock) {
-          const [addr1, addr2, addr3, addr4] = result.events.filter(
-            ({ event: { section, method } }) => (section === 'evm' && method === 'Created')
-          ).map(({ event }) => event.data[1].toHex());
+          // this is mainly for some error checking
+          handleTxResponse(result, provider.api).catch(err => {
+            console.log('❗ tx failed');
+            throw err;
+          });
 
-          if (!addr1 || !addr2 || !addr3) {
+          const [uniCoreAddr, uniRouterAddr, token0Addr, token1Addr] = result.events.filter(
+            ({ event: { section, method } }) => (section === 'evm' && method === 'Created')
+          )
+            .map(({ event }) => event.data[1].toHex());
+
+          if (!uniCoreAddr || !uniRouterAddr || !token0Addr || !token1Addr) {
             throw new Error('some perfect error handling');
           }
 
-          setUniCoreAddress(addr1);
-          setUniRouterAddress(addr2);
-          setToken0Address(addr3);
-          setToken1Address(addr4);
+          // query token balance and allowance
+          Promise.all([
+            queryTokenBalance(signer, token0Addr, claimedEvmAddress),
+            queryTokenBalance(signer, token1Addr, claimedEvmAddress),
+            queryTokenAllowance(signer, token0Addr, [claimedEvmAddress, uniRouterAddr]),
+            queryTokenAllowance(signer, token1Addr, [claimedEvmAddress, uniRouterAddr]),
+          ]).then(([b0, b1, a0, a1]) => {
+            setToken0Balance(b0);
+            setToken1Balance(b1);
+            setToken0Allowance(a0);
+            setToken1Allowance(a1);
+            setDeploying(false);
+          });
 
-          setDeploying(false);
+          setUniCoreAddress(uniCoreAddr);
+          setUniRouterAddress(uniRouterAddr);
+          setToken0Address(token0Addr);
+          setToken1Address(token1Addr);
         } else if (result.isError) {
           throw new Error('some perfect error handling');
         }
       });
-    } catch {
+    } catch (err) {
+      console.log(err);
       setDeploying(false);
     }
-  }, [signer, provider]);
-
-  // query token balance
-  useEffect(() => {
-    if (!token0Address || !token1Address || !signer) return;
-
-    (async () => {
-      const evmAddr = await signer.getAddress();
-      const balance0 = await queryTokenBalance(signer, token0Address, evmAddr);
-      const balance1 = await queryTokenBalance(signer, token1Address, evmAddr);
-
-      setToken0Balance(balance0);
-      setToken1Balance(balance1);
-    })();
-  }, [token0Address, token1Address, liquidity]);
+  }, [signer, provider, claimedEvmAddress]);
 
   /* ------------ Step 3: batch approve + add liquidity ------------ */
   const addLiquidity = useCallback(async () => {
@@ -199,51 +221,58 @@ function App() {
 
     setCalling(true);
     try {
-      const evmAddr = await signer.getAddress();
-      const extrinsic1 = await getTokenApproveExtrinsic(signer, token0Address, [uniRouterAddress, MaxUint256]);
-      const extrinsic2 = await getTokenApproveExtrinsic(signer, token1Address, [uniRouterAddress, MaxUint256]);
-      const extrinsic3 = await getAddLiquidityExtrinsic(signer, uniRouterAddress, [
-        token0Address, token1Address, 1000000, 2000000, 0, 0, evmAddr, MaxUint256,
-      ], true);
+      /* ----------
+        Step 3.1:
+          - construct each of the contract call extrinsic
+          - batch them into a single `batchAll` extrinsic
+                                                     ---------- */
+      const callExtrinsics = await Promise.all([
+        getTokenApproveExtrinsic(signer, token0Address, [uniRouterAddress, MaxUint256]),
+        getTokenApproveExtrinsic(signer, token1Address, [uniRouterAddress, MaxUint256]),
+        getAddLiquidityExtrinsic(signer, uniRouterAddress, [
+          token0Address, token1Address, input0, input1, 0, 0, claimedEvmAddress, MaxUint256,
+        ], true),
+      ]);
 
-      const extrinsic = await provider.api.tx.utility.batchAll([
-        extrinsic1,
-        extrinsic2,
-        extrinsic3,
-      ]).signAsync(await signer.getSubstrateAddress());
+      const batchCall = provider.api.tx.utility.batchAll(callExtrinsics);
 
-      extrinsic.send((result: SubmittableResult) => {
+      /* ----------
+         Step 3.2:
+           - sign and send the `batchAll` extrinsic
+           - handle result in the callback function
+                                         ---------- */
+      await batchCall.signAndSend(selectedAddress, (result: SubmittableResult) => {
         if (result.status.isFinalized || result.status.isInBlock) {
-          handleTxResponse(result, provider.api);   // TODO: mainly for some error check
-
-          const failedEvent = result.events.find(
-            ({ event: { section, method } }) => (section === 'evm' && method === 'ExecutedFailed')
-          );
-
-          if (failedEvent) {
+          // this is mainly for some error checking
+          handleTxResponse(result, provider.api).catch(err => {
             console.log('❗ tx failed');
-            throw new Error(JSON.stringify(failedEvent.event.data.toHuman()));
-          }
+            throw err;
+          });
 
+          // query pool liquidity, token balance and allowance
           Promise.all([
             queryLiquidity(signer, uniCoreAddress, [token0Address, token1Address]),
-            queryTokenAllowance(signer, token0Address, [evmAddr, uniRouterAddress]),
-            queryTokenAllowance(signer, token1Address, [evmAddr, uniRouterAddress]),
-          ]).then(([liq, a0, a1]) => {
+            queryTokenBalance(signer, token0Address, claimedEvmAddress),
+            queryTokenBalance(signer, token1Address, claimedEvmAddress),
+            queryTokenAllowance(signer, token0Address, [claimedEvmAddress, uniRouterAddress]),
+            queryTokenAllowance(signer, token1Address, [claimedEvmAddress, uniRouterAddress]),
+          ]).then(([liq, b0, b1, a0, a1]) => {
             setLiquidity(liq);
-            setToken0Allowance(a0);
-            setToken1Allowance(a1);
-
+            setToken0NewBalance(b0);
+            setToken1NewBalance(b1);
+            setToken0NewAllowance(a0);
+            setToken1NewAllowance(a1);
             setCalling(false);
           });
         } else if (result.isError) {
           throw new Error('some perfect error handling');
         }
       });
-    } catch {
+    } catch (err) {
+      console.log(err);
       setCalling(false);
     }
-  }, [signer, provider, token0Address, token1Address]);
+  }, [signer, provider, token0Address, token1Address, input0, input1]);
 
   // eslint-disable-next-line
   const ExtensionSelect = () => (
@@ -300,7 +329,7 @@ function App() {
           disabled={ connecting || !!provider }
         >
           {connecting
-            ? 'connecting ...'
+            ? <><StarOutlined spin /> connecting ...</>
             : provider
               ? `connected to ${provider.api.runtimeChain.toString()}`
               : 'connect'}
@@ -314,9 +343,9 @@ function App() {
             {loadingAccount
               ? 'loading account info ...'
               : claimedEvmAddress
-                ? (<div>claimed evm address: <span className='address'>{claimedEvmAddress}</span></div>)
-                : (<div>default evm address: <span className='address'>{signer.computeDefaultEvmAddress()}</span></div>)}
-            {balance && (<div>account balance: <span className='address'>{balance}</span></div>)}
+                ? (<div>claimed evm address: <span className='grayLight'>{claimedEvmAddress}</span></div>)
+                : (<div>default evm address: <span className='grayLight'>{signer.computeDefaultEvmAddress()}</span></div>)}
+            {balance && (<div>account balance: <span className='grayLight'>{balance}</span></div>)}
           </div>
         )}
       </section>
@@ -330,65 +359,80 @@ function App() {
           onClick={ deploy }
         >
           { uniRouterAddress
-            ? 'all contracts deploy'
+            ? 'all contracts deployed'
             : deploying
-              ? 'deploying all 4 contracts together ...'
+              ? <><StarOutlined spin /> deploying 4 contracts together ...</>
               : 'batch deploy'}
         </Button>
 
         { uniRouterAddress && (
           <>
-            <div>Uni Core address: <span className='address'>{ uniCoreAddress }</span></div>
-            <div>Uni Router address: <span className='address'>{ uniRouterAddress }</span></div>
-            <div>TokenA address: <span className='address'>{ token0Address }</span></div>
-            <div>TokenB address: <span className='address'>{ token1Address }</span></div>
-            <div>TokenA balance: <span className='address'>{ token0Balance }</span></div>
-            <div>TokenB balance: <span className='address'>{ token1Balance }</span></div>
-            <div>TokenA allowance: <span className='address'>{ token0Allowance }</span></div>
-            <div>TokenB allowance: <span className='address'>{ token1Allowance }</span></div>
+            <div>Uni Core address: <span className='grayLight'>{ uniCoreAddress }</span></div>
+            <div>Uni Router address: <span className='grayLight'>{ uniRouterAddress }</span></div>
+            <div>TokenA address: <span className='grayLight'>{ token0Address }</span></div>
+            <div>TokenB address: <span className='grayLight'>{ token1Address }</span></div>
+            <div>TokenA balance: <span className='grayLight'>{ token0Balance }</span></div>
+            <div>TokenB balance: <span className='grayLight'>{ token1Balance }</span></div>
+            <div>TokenA allowance: <span className='grayLight'>{ token0Allowance }</span></div>
+            <div>TokenB allowance: <span className='grayLight'>{ token1Allowance }</span></div>
+            <div>Dex liquidity: <span className='grayLight'>0x00</span></div>
           </>
         )}
       </section>
 
       { /* ------------------------------ Step 3 ------------------------------*/}
       <section className='step'>
-        <div className='step-text'>Step 3: Batch Approve & Add Liquidity { liquidity !== '' && <Check /> }</div>
+        <div className='step-text'>Step 3: Batch Approve & Add Liquidity { liquidity !== '0' && <Check /> }</div>
+        <Input
+          type='text'
+          disabled={ uniRouterAddress === '' || calling || liquidity !== '0' }
+          value={ input0 }
+          onChange={ e => setInput0(e.target.value) }
+          addonBefore='token A amount'
+        />
+        <Input
+          type='text'
+          disabled={ uniRouterAddress === '' || calling || liquidity !== '0' }
+          value={ input1 }
+          onChange={ e => setInput1(e.target.value) }
+          addonBefore='token B amount'
+        />
         <Button
           type='primary'
-          disabled={ uniRouterAddress === '' || liquidity !== '' }
+          disabled={ uniRouterAddress === '' || calling || liquidity !== '0' }
           onClick={ addLiquidity }
         >
-          { liquidity !== ''
+          { liquidity !== '0'
             ? 'liquidity added'
             : calling
-              ? 'sending batch calls ...'
+              ? <><StarOutlined spin /> processing batch calls ...</>
               : 'approve tokens + add liquidity'}
         </Button>
 
-        { liquidity !== '' && (
+        { liquidity !== '0' && (
           <>
-            <div>TokenA balance: <span className='address'>{ token0Balance }</span></div>
-            <div>TokenB balance: <span className='address'>{ token1Balance }</span></div>
-            <div>Dex liquidity: <span className='address'>{ liquidity }</span></div>
-            <div>TokenA allowance: <span className='address'>{ token0Allowance }</span></div>
-            <div>TokenB allowance: <span className='address'>{ token1Allowance }</span></div>
+            <div>TokenA balance: <span className='grayLight'>{ token0NewBalance }</span><span className='redLight'><ArrowDownOutlined /> { Number(token0Balance) - Number(token0NewBalance) }</span></div>
+            <div>TokenB balance: <span className='grayLight'>{ token1NewBalance }</span><span className='redLight'><ArrowDownOutlined /> { Number(token1Balance) - Number(token1NewBalance) }</span></div>
+            <div>TokenA allowance: <span className='grayLight'>{ token0NewAllowance }</span><span className='greenLight'><ArrowUpOutlined /> ♾️</span></div>
+            <div>TokenB allowance: <span className='grayLight'>{ token1NewAllowance }</span><span className='greenLight'><ArrowUpOutlined /> ♾️</span></div>
+            <div>Dex liquidity: <span className='grayLight'>{liquidity}</span><span className='greenLight'><ArrowUpOutlined /> { liquidity }</span></div>
           </>
         )}
       </section>
 
-      { liquidity !== '' && (
+      { liquidity !== '0' && (
         <section className='step' id='congrats'>
           <div>Congratulations 🎉🎉</div>
-          <div>You have succesfully setup uniswap with <span className='decorate'>ONE</span> signature</div>
-          <div>Interacted with contracts via <span className='decorate'>BATCH</span> transations</div>
+          <div>You have succesfully deployed tokens and setup uniswap with <span className='decorate'>ONE</span> signature</div>
+          <div>and interacted with them via <span className='decorate'>BATCH</span> contract calls</div>
           <div>Powered by <span className='decorate'>Acala EVM+</span></div>
-          {/* <Button
-            id='next-level'
-            type='primary'
-            onClick={ () => window.open('https://github.com/AcalaNetwork/bodhi-examples/tree/master/batch-transactions', '_blank') }
-          >
-            Take Me To Advanced Example (Coming Soon)
-          </Button> */}
+
+          <Confetti
+            numberOfPieces={ 800 }
+            tweenDuration={ 20000 }
+            gravity={ 0.2 }
+            recycle={ false }
+          />
         </section>
       )}
     </div>
--- batch-transactions/src/utils.ts
+++ batch-transactions/src/utils.ts
@@ -1,4 +1,4 @@
-import { ContractFactory, Contract, BigNumber } from 'ethers';
+import { ContractFactory, Contract, BigNumber, ContractInterface } from 'ethers';
 import { Signer } from '@acala-network/bodhi';
 import { toBN } from './utils-from-bodhi';
 
@@ -7,53 +7,59 @@ import uniRouterContract from './UniswapV2Router.json';
 import IUniswapV2Pair from './IUniswapV2Pair.json';
 import tokenContract from './Token.json';
 
-// TODO: add and export this util in bodhi
-export const getDeployExtrinsic = async (signer: Signer, abi: any, bytecode: string, args: any[] = []) => {
+// TODO: maybe export these 2 factory from bodhi?
+/* --------------------------------------------------------
+                 deployment extrinsic factory
+----------------------------------------------------------- */
+export const getDeployExtrinsic = async (signer: Signer, abi: ContractInterface, bytecode: string, args: any[] = [], value = 0) => {
+  const evmAddress = await signer.getAddress();
   const factory = new ContractFactory(abi, bytecode, signer);
 
-  const evmAddress = await signer.getAddress();
-  const tx = await signer.populateTransaction({
-    ...factory.getDeployTransaction(...args),
-    from: evmAddress,
-  });
+  const { data, to } = factory.getDeployTransaction(...args);
 
   const {
     gas: gasLimit,
     storage: storageLimit,
-  } = await signer.provider.estimateResources(tx);
+  } = await signer.provider.estimateResources({
+    from: evmAddress,
+    to,
+    data,
+  });
 
   return signer.provider.api.tx.evm.create(
-    tx.data,
-    toBN(tx.value),
+    data,
+    toBN(value),
     toBN(gasLimit),
     toBN(storageLimit.isNegative() ? 0 : storageLimit),
-    tx.accessList || []
+    []
   );
 };
 
-export const getCallExtrinsic = async (signer: Signer, abi: any, contractAddr: string, method: string, args: any[] = [], force = false) => {
-  console.log('getCallExtrinsic', args)
+/* --------------------------------------------------------
+                   call extrinsic factory
+----------------------------------------------------------- */
+export const getCallExtrinsic = async (signer: Signer, abi: ContractInterface, contractAddr: string, method: string, args: any[] = [], value = 0, force = false) => {
+  const evmAddress = await signer.getAddress();
   const contract = new Contract(contractAddr, abi, signer);
   const data = contract.interface.encodeFunctionData(method, args);
 
-  const evmAddress = await signer.getAddress();
-  // console.log('!!!!!!!!!!!!')
-  // const tx = await signer.populateTransaction({
-  //   data,
-  //   to: contractAddr,
-  //   from: evmAddress,
-  // });
-
-  // console.log(tx)
-
+  // default gasLimit and storageLimit
   let gasLimit = BigNumber.from(21000000);
   let storageLimit = BigNumber.from(64000);
+
   try {
+    /* --------------------
+       default gasLimit is big, so if we batch many tx
+       might encounter `gasLimit exceeds block gas limit`
+       so we try to get a better estimation of gas params
+       this step could fail if the tx depends on the previous tx in the batch
+       in which case we can force sending it using the default gas params
+                                                         -------------------- */
     const { gas, storage } = await signer.provider.estimateResources({
       data,
       to: contractAddr,
       from: evmAddress,
-      value: 0,
+      value,
     });
 
     gasLimit = gas;
@@ -69,23 +75,23 @@ export const getCallExtrinsic = async (signer: Signer, abi: any, contractAddr: s
   return signer.provider.api.tx.evm.call(
     contractAddr,
     data,
-    toBN(0),
+    toBN(value),
     toBN(gasLimit),
-    toBN(storageLimit),
+    toBN(storageLimit.isNegative() ? 0 : storageLimit),
     []
   );
 };
 
-/* ----------------------------
-     extrinsic constructors
-------------------------------- */
-export const getUniFactoryDeployExtrinsic = async (signer: Signer, addr: string) => (
-  getDeployExtrinsic(signer, uniFactoryContract.abi, uniFactoryContract.bytecode, [addr])
+/* --------------------------------------------------------
+                  extrinsic constructors
+----------------------------------------------------------- */
+export const getUniFactoryDeployExtrinsic = async (signer: Signer, deployer: string) => (
+  getDeployExtrinsic(signer, uniFactoryContract.abi, uniFactoryContract.bytecode, [deployer])
 );
 
 const DUMMY_WETH_ADDR = '0xB7d729C983b819611E68DAee71b4A2C950f86dc8';
-export const getUniRouterDeployExtrinsic = async (signer: Signer, factoryAddr:string) => (
-  getDeployExtrinsic(signer, uniRouterContract.abi, uniRouterContract.bytecode, [factoryAddr, DUMMY_WETH_ADDR])
+export const getUniRouterDeployExtrinsic = async (signer: Signer, uniCoreAddr:string) => (
+  getDeployExtrinsic(signer, uniRouterContract.abi, uniRouterContract.bytecode, [uniCoreAddr, DUMMY_WETH_ADDR])
 );
 
 export const getTokenDeployExtrinsic = async (signer: Signer, args: any[]) => (
@@ -97,12 +103,12 @@ export const getTokenApproveExtrinsic = async (signer: Signer, contractAddr: str
 );
 
 export const getAddLiquidityExtrinsic = async (signer: Signer, contractAddr: string, args: any[], force = false) => (
-  getCallExtrinsic(signer, uniRouterContract.abi, contractAddr, 'addLiquidity', args, force)
+  getCallExtrinsic(signer, uniRouterContract.abi, contractAddr, 'addLiquidity', args, 0, force)
 );
 
-/* ----------------------------
-          query helpers
-------------------------------- */
+/* --------------------------------------------------------
+                      query helpers
+----------------------------------------------------------- */
 export const queryTokenBalance = async (signer: Signer, tokenAddr: string, addr: string): Promise<string> => {
   const contract = new Contract(tokenAddr, tokenContract.abi, signer);
 
